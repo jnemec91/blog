d98fa776568aa439e780c1f077411f2f0a859f3a
Jaroslav Nemec <j.nemecc9@gmail.com>
2026-01-27 15:33:35 +0100

    Refactor production settings to use environment variable for SECRET_KEY

--- personal_site/personal_site/settings/production.py
+++ personal_site/personal_site/settings/production.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5g)c&!mry6lblot71ergk#8on$z@ch!n0w512r#be=92si33%p'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
