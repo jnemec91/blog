f2152e6b86d222bf747614ee4430c1eb0b36032a
Jaroslav Nemec <j.nemecc9@gmail.com>
2026-01-27 13:02:56 +0100

    add current year context processor for templates

--- personal_site/blog/context_processors.py
+++ personal_site/blog/context_processors.py
@@ -1,7 +1,14 @@
 from .models import Category
+from datetime import datetime
 
 def navbar_context(request):
     """Provide navbar categories to all templates."""
     return {
         'categories': Category.objects.filter(blogpost__is_published=True).distinct()
+    }
+
+def current_year(request):
+    """Provide the current year to all templates."""
+    return {
+        'current_year': datetime.now().year
     }
