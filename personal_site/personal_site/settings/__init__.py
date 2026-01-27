e07b5d13410b9f51420077438cfc72d55e773767
Jaroslav Nemec <j.nemecc9@gmail.com>
2026-01-27 15:31:07 +0100

    Add print statements to indicate loaded settings for production and development

--- personal_site/personal_site/settings/__init__.py
+++ personal_site/personal_site/settings/__init__.py
@@ -2,5 +2,7 @@ import os
 
 if os.environ.get('ENVIRONMENT') == 'production':
     from .production import *
+    print("Loaded production settings")
 else:
     from .development import *
+    print("Loaded development settings")
